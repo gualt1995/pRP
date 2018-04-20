342ffb053dd727094b6cb17bf680aae28cb20552
Gualtiero MOTTOLA <32741397+gualt1995@users.noreply.github.com>
2018-04-20 19:57:19 +0200

    genetic algorithm working

--- graphTools.py
+++ graphTools.py
@@ -17,7 +17,7 @@ def graphloader(filename):
         else:
             words = line.split()
             if words[0] == "E":
-                G.add_edge(words[1], words[2], weight=words[3])
+                G.add_edge(words[1], words[2], weight=int(words[3]))
             elif words[0] == "END":
                 build = False
         if term:
@@ -29,15 +29,45 @@ def graphloader(filename):
     return G
 
 
-def kurskal(graph):
-    nx.minimum_spanning_tree(graph)
+def kruskal(graph):
+    return nx.minimum_spanning_tree(graph)
 
 
+def buildgraphofsolution(solution, graph):
+    G = nx.Graph(graph)
+    for i in range(0, len(solution)):
+        if solution[i] == '0':
+            G.remove_node(str(i+1))
+    return G
+
+
+def sizeofsolution(graph):
+    nbnodes = len(graph)
+    nodeslist = list(graph.nodes.data('term'))
+    cpt = 0
+    for node in nodeslist:
+        if node[1]:
+            cpt += 1
+    return nbnodes - cpt
+
+def fitnessevaluation(solution, graph):
+    m = 50
+    subgraph = buildgraphofsolution(solution, graph)
+    kurskalsub = kruskal(subgraph)
+    res = 0
+    for edge in nx.get_edge_attributes(kurskalsub, "weight").values():
+        res += edge
+    if nx.is_connected(kurskalsub):
+        return res
+    else:
+        nbnodes = len(kurskalsub)
+        nbedges = kurskalsub.number_of_edges()
+        return res + m * ((nbnodes - 1) - nbedges)
 graph = graphloader("B/b01.stp")
 #print(graph.nodes.data())
 #for e in graph.edges.data():
 #    print(e)
-print(test)
+print("test")
 #print(graph.number_of_nodes())
 #nx.draw(graph)
 #plt.show()
--- tool.py
+++ tool.py
@@ -1,6 +1,25 @@
 import networkx as nx
 import matplotlib.pylab as plt
 import random
+import graphTools as gt
+import operator
+
+
+def fullgeneration(nbgen, size, proba):
+    res = list()
+    for i in range(0,nbgen):
+        res.append(generation(size, proba))
+    return res
+
+
+def generation(size,proba):
+    res = ""
+    for bit in range(size):
+        if random.randint(1,101) < proba*100:
+            res += "1"
+        else:
+            res += "0"
+    return res
 
 
 def mutation(solution):
@@ -16,13 +35,65 @@ def mutation(solution):
     return res
 
 
-def generation(size,proba):
-    res = ""
-    for bit in range(size):
-        if random.randint(1,101) < proba:
-            res += "1"
-        else:
-            res += "0"
-    return res
+# returns two selected parents(res1 and res2 are tuples (solution,proba_rating))
+def selection(popratingsorted):
+    res1 = res2 = ""
+    p1 = random.random()
+    p2 = random.random()
+    for j in popratingsorted:
+        if j[1] < p1:
+            res1 = j
+            break
+    for j in popratingsorted:
+        if j[1] < p2:
+            res2 = j
+            break
+    return res1, res2
 
-print(mutation("00001111"))
+
+def croissement(parent1,parent2,pcroissement):
+    #TODO on coupe a la moitee pas sure que c'est bien ca
+    point = int(len(parent1))
+    length = len(parent1)-1
+    p1 = random.random()
+    if pcroissement < p1:
+        firstpar1, secondpar1 = parent1[0][:int(len(parent1[0]) / 2)], parent1[0][int(len(parent1[0]) / 2):]
+        firstpar2, secondpar2 = parent2[0][:int(len(parent2[0]) / 2)], parent2[0][int(len(parent2[0]) / 2):]
+        parent1 = (firstpar1+secondpar2, parent1[1])
+        parent2 = (firstpar2+secondpar1, parent2[1])
+    return parent1, parent2
+
+
+
+
+
+def genetic_algorithm(graph,steps,popsize,pstart,pselect,pcroissement):
+    sollen = gt.sizeofsolution(graph)
+    pop = fullgeneration(popsize, sollen, pstart)
+    poprating = dict()
+    for k in range(0, steps):
+        newpop = list()
+        for i in range(0, popsize):
+            poprating[pop[i]] = gt.fitnessevaluation(pop[i], graph)
+        popratingsorted = sorted(poprating.items(), key=operator.itemgetter(1))
+        popproba = list()
+        for i in range(0,len(popratingsorted)):
+            popproba.append((popratingsorted[i][0],pselect*pow((1-pselect), i)))
+        for i in range(0, int(popsize/2)):
+            # version ou on ajoute que les fils a la solution;
+            parent1, parent2 = selection(popproba)
+            fils1, fils2 = croissement(parent1, parent2, pcroissement)
+            fils1m = (mutation(fils1[0]), fils1[1])
+            fils2m = (mutation(fils2[0]), fils2[1])
+            newpop.append(fils1m[0])
+            newpop.append(fils2m[0])
+        pop = newpop;
+        print(newpop[0])
+    for i in range(0, popsize):
+        poprating[pop[i]] = gt.fitnessevaluation(pop[i], graph)
+    popratingsorted = sorted(poprating.items(), key=operator.itemgetter(1))
+    return popratingsorted[-1]
+
+
+graph = gt.graphloader("B/b01.stp")
+print(genetic_algorithm(graph, 50, 10, 0.2, 0.8, 0.1))
